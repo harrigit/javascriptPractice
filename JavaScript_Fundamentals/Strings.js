f722f8f8249c1778ebcc21b1e0da30736ea9aa26
harrigit <harisniazi302@gmail.com>
2023-02-13 17:17:07 +0500

    Strings And Slice Concepts

---
+++ JavaScript_Fundamentals/Strings.js
@@ -0,0 +1,29 @@
+//template literals
+//modren JS
+let Name = "uzair";
+let Name2 = "Ahmad";
+console.log(`${Name} is better Developer than ${Name2}`);//this is actullay Called String Interpulation Interview Question
+
+//Strings 
+let Nam = "Muhammad Haris Khan";
+console.log(Nam.toLowerCase());
+console.log(Nam.toUpperCase());
+console.log(Nam.slice(3, 7));
+console.log(Nam.replace("Muhammad Haris Khan", "Muhammad Hashir Khan"));
+
+//Trim Method
+let mm = "     Muhammad      Haris      Khan";
+console.log(mm.trim());
+console.log(mm)
+
+//practice Questions
+//Includes In javaScript
+let words="My Name Is Muhammad Haris Khan Niazi. I am From Bhakkar and I am software engineer and Currently working in ICOMMUNIX Ltd."
+let text = words.includes("Haris");
+console.log(text);
+
+//USe of Slice in JS //vvvvvvimp
+
+
+let Nae="haris Khan";
+console.log(Nae.slice(3));
