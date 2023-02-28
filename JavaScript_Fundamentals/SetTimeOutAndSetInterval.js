871ccf687997e6968c52f2fcc4087710980f2cfa
harrigit <harisniazi302@gmail.com>
2023-02-28 11:21:57 +0500

    Callbacks And SetTimeout

---
+++ JavaScript_Fundamentals/SetTimeOutAndSetInterval.js
@@ -0,0 +1,11 @@
+//SetTimeOut In js
+alert("hello UStad Kia hal ha I am Waiting for your Response!");
+setTimeout(() => {
+    alert("ma Theak hoon tum sunao kasa ho");
+}, 5000);
+
+//SetInterval In Js
+alert(" I will Run untill You after Every Second");
+setInterval(() => {
+    console.log("I am Enjoying ");
+}, 2000);
