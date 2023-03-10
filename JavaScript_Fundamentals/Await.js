3b4e3983b6ff8aed98b8865bf506d562117e3e39
harrigit <harisniazi302@gmail.com>
2023-03-10 11:23:32 +0500

    Await And Error Handling

--- JavaScript_Fundamentals/Await.js
+++ JavaScript_Fundamentals/Await.js
@@ -1,8 +1,8 @@
 async function Check(){
     console.log("1: Checking.........");
-    console.log("2: Checking.........");
+    await console.log("2: Checking.........");
     console.log("3: Checking.........");
-    await console.log("5000: Checking.........");
+    console.log("5000: Checking.........");
 }
 console.log("4: Checking.........");
 Check();
---
+++ JavaScript_Fundamentals/ErrorHandling.js
@@ -0,0 +1,9 @@
+try {
+    console.log("Checking")
+
+error
+    console.log("Checked")
+} catch(error) {
+    console.log("Failed To Check Because of error");
+    console.log(error)
+}
